28eefc3b5a0a44b76926293a89e4cf9bb7aada09
Cubix25 <129642928+Cubix25@users.noreply.github.com>
2023-04-17 21:39:25 -0400

    Add files via upload

--- MainScript.js
+++ MainScript.js
@@ -55,17 +55,21 @@ var Easy = [
 
     ["True","False","","","T/F: Gift giving is a secular way Christmas is celebrated"],
 
-    /*["Cor","fal","fal","fal","Quest"],
+    ["False","True","","","T/F: All the cristian denominations believe in the pope."],
 
-    ["Cor","fal","fal","fal","Quest"],
+    ["A manger","A hotel","A barn","A 5 star hilton","Where was Jesus born?"],
 
-    ["Cor","fal","fal","fal","Quest"],
+    ["A visit to a holy place or a religious event","A visit to any church in your local area","A visit to a fun holiday destination","A visit to an ancestral land","Pilgrimage is:"],
 
-    ["Cor","fal","fal","fal","Quest"],
+    ["Every sunday","Every 1st sunday of the month","Every day in the morning","Every friday evening","When does Liturgy take place?"],
 
-    ["Cor","fal","fal","fal","Quest"],
+    ["True","False","","","T/F: Sacraments are an ‘Outward sign of given grace."],
 
-    ["Cor","fal","fal","fal","Quest"],*/
+    ["3","5","7","25","How many main topics of sacraments are there?"],
+
+    ["Original Sin","Being born","Dirt","Bad omens","Baptism cleanses the soul of what?"],
+
+    ["Orthodox/Roman celebrate 7, Protestants celebrate 2","Orthodox/Roman celebrate 2, Protestant celebrate 7","Orthodox/Roman celebrate 6, Protestants celebrate 3","Both celebrate 7","Orthodox/Roman Catholics and Protestants celebrate different sacraments, how many do each celebrate?"],
 ]
 
 var Medium = [
@@ -90,6 +94,48 @@ var Medium = [
     ["False","True","","","T/F: The Apostles were given the gift of tongue from Jesus"],
 
     ["False","True","","","T/F: Christmas is often referred to as the birthday of the church"],
+
+    ["God incarnate","A guy","A carpenter","A cool guy","Who was Jesus?"],
+
+    ["False","True","","","T/F: Do all christian denominations belive in purgatory?"],
+
+    ["There is one God","Purgatory is a thing"," The unity of the trinity","there is original sin","What is a universal christian belief?"],
+
+    ["False","True","","","T/F: The bible is entirely true."],
+
+    ["Keep the holy trinity holy","You shall not have other Gods before me","You shall not commit adultery","You shall not steal","What is NOT one of the commandments?"],
+
+    ["Moses","God","Jesus","John","Who was the messenger of the 10 commandments?"],
+
+    ["Judaism","Buddasim","Monotheistic","Catholicism ","What religion created christianity?"],
+
+    ["Lord’s Prayer","Apostles Creed","Oh, it’s just a necklace","Hail Mary","What prayer does a Rosary most widely represent?"],
+
+    ["Baptismal Font","Heaven","Swimming pool","Baptistery","Where do baptisms take place in a Roman Catholic Church?"],
+
+    ["True","False","","","T/F: “Eucharist”, “Mass”, “Liturgy”, are all names for different sacraments."],
+
+    ["40","30","28","He never fasted","Jesus fasted for ___ days in the wilderness."],
+
+    ["All of these are true","To stay away from specific foods","It’s a traditional practise","To seek pardon from misdeeds","Many fasted in the remembrance of Jesus:"],
+
+    ["10% of their income","15% of their income","50% of their income","As much as they can","What is the expected donation each family should make to the church financially?"],
+
+    ["Tithe","Donation","Tenth","Gratuity","What is a required donation called?"],
+
+    ["False","True","","","Men and women are allowed to sit together in an Eastern Orthodox Church."],
+
+    ["True","False","","","Men and women are allowed to sit together in an Roman Catholic Church"],
+
+    ["Initiation, Vocation, Healing","Healing, Vocation, Initiation","Vocation, Initiation, Healing","Initiation, Healing, Vocation","What is the correct order of the sacraments?"],
+
+    ["Orthodox are infants and get poured, Protestants are older and get dunked","Orthodox baptisms are older and get dunked, Protestant are infants and get poured","They follow the same practices","","Rituals in Baptism include"],
+
+    ["Beliefs that bread and wine change their substance","40 days after easter Jesus gives his disciples gifts","Place worship on churches","Letter sent to all churches from Bishop","Transubstantiation is what?"],
+
+    ["False","True","","","T/F: During Reconciliation, the Priest can tell others about your confessions"],
+
+    ["Yes","No","Maybe","Idk","Are all 3 practices allowed to anoint the sick?"],
 ]
 
 var Hard = [
@@ -114,6 +160,32 @@ var Hard = [
     ["The mortality of Christians (and all humans)","That Jesus died on the cross for our sins","The body and blood of Christ","The suffering of Christ on the cross","The mark of ashes on the forehead on Ash Wednesday represents/symbolizes…"],
 
     ["True","False","","","T/F: Pentecost marks the day in which the Wise Men visited Jesus"],
+
+    ["People","Animals","Plants","Night","What was the last thing God created in the creation story?"],
+
+    ["To remind them of baptism","For holy protection","To remind them of the holy trinity","because they are hot","Why do catholics bless themselves with holy water?"],
+
+    ["The word of God","A book written by Gods followers"," A book about jesus life","A book that explains the world around us","What is the bible? (Most correct answer)"],
+
+    ["Jesus's teachings","The disciples teachings","The teachings of the Catholic church","The teachings of the Catholic church","What are the beatitudes?"],
+
+    ["The Apostles Creed","The Nicene creed","The Assasins creed","Creed 3 (The one with Micheal B. Jordan)","What is the creed used in western liturgy?"],
+
+    ["Behind the royal door","5 feet to the left from the entrance","In the church","Tabernacle","Where is the altar placed in an Eastern Orthodox Church?"],
+
+    ["Petition","Thanksgiving","Praise","Intercession","“Hey God, It’s me. Can I please get an XBOX for my birthday? Please, please, please?” What kind of prayer is this?"],
+
+    ["Reflecting upon the bible to bring yourself closer to god","Staring at the bible for a long time","Doing anything without putting thought into it","Reading the bible everyday","Contemplative meditation is:"],
+
+    ["Refers to the Virgin Mary","Is Jesus’s aunt","None of these answers are correct","Is not linked to Christianity","Our Lady of Fatima:"],
+
+    ["Talking to yourself","Intercession","Petition","Thanksgiving","Which of the following are NOT a form of christian prayer:"],
+
+    ["Knowledge, Wisdom","Counsel, Fortitude","Wisdom, Faith","Knowledge, Fear of the Lord","What gifts are bestowed during confirmation"],
+
+    ["False","True","","","T/F: You are allowed to get re-married in the catholic church?"],
+
+    ["Supper table was replaced by an alter","Church added more scarements to keep with times","Rituals were added","All sacraments are given at the same time for all","As the church evolved, what changed?"],
 ]
 
 var Hellish = [
@@ -134,9 +206,17 @@ var Hellish = [
     ["Jesus visited his apostles and commissioned them to baptize all nations and spread his teachings","The Holy Spirit empowered the apostles with various spiritual gifts and abilities","The apostles were given the ability to speak other languages","None of these events happened","Which of the following DID NOT take place on Pentecost?"],
 
     ["The Wise Men","The Gospel Matthew","Jesus’ disciple Mary Magdalene","Jesus Mother, Mary","The Magi refers to…"],
+
+    ["Killing of firstborn","Locusts","Darkness","Lice","What was the last plague on egypt?"],
+
+    ["All of these answers are correct","Prayers, scripture readings, and singing of psalms","Consecration of wine and bread","Communions and collections","What were the main elements of worship that were established by the second century?"],
+
+    ["Triple procession around alter","Grace of God is bestowed upon couple","Include hymns, liturgies","Spongbob","Which rituals are practiced by Orthodoxs during Marriage?"],
+
+    ["Extensive education and formation","Laying of the fists","Formal clergies within the church","Videogame","Ordination involves what?"]
 ]
 
-//["Cor","fal","fal","fal","Quest"]
+console.log(Easy.length + Medium.length + Hard.length + Hellish.length);
 
 /*Reset btns and text*/
 var questBtns =
